ec8d22a80d15612d8f90b805947136e3f9e9c767
DC <DC@10.48.33.10>
2025-02-11 14:37:18 -0500

    Created players and wargame

---
+++ Gameproject/src/gameproject/Gameproject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gameproject;
+
+/**
+ *
+ * @author ayushi goswami
+ */
+public class Gameproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WarGame game = new WarGame("Player 1", "Player 2");
+        game.startGame();    }
+    
+}
